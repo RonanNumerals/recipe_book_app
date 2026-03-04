79d9e4b99b8970fe7da7f3841f45ba00c56074b7
Ronan Pelot <ronanpelot@gmail.com>
2026-03-03 22:16:46 -0500

    Added more recipes and an app bar to home page

--- lib/data/recipes_data.dart
+++ lib/data/recipes_data.dart
@@ -10,7 +10,7 @@ final List<Recipe> sampleRecipes = [
   Recipe(
     name: 'Chicken Quesadilla',
     imagePath: 'assets/images/chicken_quesadilla.jpg',
-    ingredients: ['Tortillas', 'Chicken', 'Cheese', 'Shallot', 'Garlic', 'Jalapeno'],
+    ingredients: ['Tortillas', 'Chicken', 'Cheese', 'Shallot', 'Garlic', 'Jalapeno', 'Butter', 'Salt', 'Garlic powder', 'Cumin'],
     instructions: 'Season chicken and cook. Cook shallot, garlic, and jalapeno. Butter pan and add tortilla. Layer chicken, cheese, and shallot mixture. Top with another tortilla. Cook until golden and cheese is melted.',
   ),
   Recipe(
@@ -23,6 +23,18 @@ final List<Recipe> sampleRecipes = [
     name: 'Chicken Curry',
     imagePath: 'assets/images/chicken_curry.jpg',
     ingredients: ['Chicken', 'Curry paste', 'Onion', 'Garlic', 'Rice'],
-    instructions: 'Season chicken with curry powder and cook with onion and garlic. Thaw Curry paste. Serve with rice.',
+    instructions: 'Season chicken with curry powder and cook with onion and garlic. Thaw curry paste and warm in microwave. Serve with rice.',
+  ),
+  Recipe(
+    name: 'French Toast',
+    imagePath: 'assets/images/french_toast.jpg',
+    ingredients: ['Bread', 'Eggs', 'Milk', 'Cinnamon', 'Butter', 'Sugar', 'Syrup'],
+    instructions: 'Whisk eggs, milk, sugar, and cinnamon. Dip bread in mixture. Cook in butter until golden. Enjoy with syrup.',
+  ),
+  Recipe(
+    name: 'Beef Tacos',
+    imagePath: 'assets/images/tacos.webp',
+    ingredients: ['Beef', 'Flour tortillas', 'Lettuce', 'Avocado', 'Cheese', 'Taco seasoning'],
+    instructions: 'Cook beef with taco seasoning. Mash avocado into a bowl, add salt and pepper. Fill tortilla with beef and toppings.',
   ),
 ];
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,8 +11,8 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text('Recipe Book')),
       body: Column(
-        //mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Expanded(
             child: ListView.builder(
